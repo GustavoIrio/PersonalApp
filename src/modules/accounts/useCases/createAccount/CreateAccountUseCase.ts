704d9c2146d0e4dbb1beb4518e3c2205fbf0982c
Gustavo Buss <gustavoirio@hotmail.com>
2022-10-10 16:03:36 -0300

    config build

--- src/modules/accounts/useCases/createAccount/CreateAccountUseCase.ts
+++ src/modules/accounts/useCases/createAccount/CreateAccountUseCase.ts
@@ -1,6 +1,4 @@
 import { firebaseAdmin } from "../../../../database/Firebase-admin";
-// import { hash } from 'bcrypt'
-// import { prisma } from "../../../../database/PrismaClient";
 
 interface ICreateAccount {
     fullName: string;
@@ -10,28 +8,6 @@ interface ICreateAccount {
 
 export class CreateAccountUseCase {
     async execute({ fullName, email, password }: ICreateAccount) {
-        // // if exists at prisma
-        // const clientExists = await prisma.user.findUnique({
-        //     where: {
-        //         email
-        //     }
-        // });
-        
-        // if (clientExists) {
-        //     throw new Error("The email address is already in use by another account.")
-        // }
-        
-        // const hashPassword = await hash(password, 10);
-
-        // // saving at prisma
-        // const user = await prisma.user.create({
-        //     data: {
-        //         name: fullName,
-        //         email,
-        //         password: hashPassword,
-        //     }
-        // })
-
         // saving at firebase auth
         const userFirebase = await firebaseAdmin.auth()
             .createUser({
