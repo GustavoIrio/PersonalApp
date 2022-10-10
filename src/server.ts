2dac33e10b713d6c2e6880b6ec972f356de72e40
Gustavo Buss <gustavoirio@hotmail.com>
2022-10-10 19:18:16 -0300

    commit changes

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use(cors);
+app.use(cors());
 
 // Middleware to show errors
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
