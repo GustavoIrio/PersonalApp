8fbc903479fe1d846b34e33086be9259a792dad1
Gustavo Buss <gustavoirio@hotmail.com>
2022-10-10 21:43:51 -0300

    code corrections

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,27 @@ import cors from "cors";
  
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use(routes);
 
-app.use(cors());
+// app.use(cors({
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+// }));
+
+// app.all('*', function(req, res) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+//     res.setHeader('Access-Control-Allow-Credentials', 'true'); // If needed
+
+//     res.send('cors problem fixed:)');
+// });
 
 // Middleware to show errors
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
